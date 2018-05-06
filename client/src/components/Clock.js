09140acfac95f5a86619fb59676257af03980184
Jeremy Han <jeremyhan111@gmail.com>
2018-05-06 11:47:17 +0800

    added bounce

--- client/src/components/Clock.js
+++ client/src/components/Clock.js
@@ -43,8 +43,62 @@ class Clock extends React.Component {
 	  }
 	}
 
+	/*
+	 * Do the first minute's rotation
+	 */
+	moveMinuteHands(containers) {
+	  for (var i = 0; i < containers.length; i++) {
+	    containers[i].style.webkitTransform = 'rotateZ(6deg)';
+	    containers[i].style.transform = 'rotateZ(6deg)';
+	  }
+	  // Then continue with a 60 second interval
+	  setInterval(function() {
+	    for (var i = 0; i < containers.length; i++) {
+	      if (containers[i].angle === undefined) {
+	        containers[i].angle = 12;
+	      } else {
+	        containers[i].angle += 6;
+	      }
+	      containers[i].style.webkitTransform = 'rotateZ('+ containers[i].angle +'deg)';
+	      containers[i].style.transform = 'rotateZ('+ containers[i].angle +'deg)';
+	    }
+	  }, 60000);
+	}
+
+	/*
+	 * Move the second containers
+	 */
+	moveSecondHands() {
+	  var containers = document.querySelectorAll('.seconds-container');
+	  setInterval(function() {
+	    for (var i = 0; i < containers.length; i++) {
+	      if (containers[i].angle === undefined) {
+	        containers[i].angle = 6;
+	      } else {
+	        containers[i].angle += 6;
+	      }
+	      containers[i].style.webkitTransform = 'rotateZ('+ containers[i].angle +'deg)';
+	      containers[i].style.transform = 'rotateZ('+ containers[i].angle +'deg)';
+	    }
+	  }, 1000);
+	}
+
+	setUpMinuteHands() {
+	  // Find out how far into the minute we are
+	  var containers = document.querySelectorAll('.minutes-container');
+	  var secondAngle = containers[0].getAttribute("data-second-angle");
+	  if (secondAngle > 0) {
+	    // Set a timeout until the end of the current minute, to move the hand
+	    var delay = (((360 - secondAngle) / 6) + 0.1) * 1000;
+	    setTimeout(() => {this.moveMinuteHands(containers)}, delay);
+	  }
+	}
+
+
 	componentDidMount() {
 		this.initLocalClocks();
+		this.setUpMinuteHands();
+		this.moveSecondHands();
 	}
 
 	render() {
