9c6d7165f9382840563efc23eea4c0c17a9a4a51
Jeremy Han <jeremyhan111@gmail.com>
2018-05-10 00:12:35 +0800

    added core values and history to about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,12 +9,10 @@ const About = () => {
 				title="About Us"
 			/>
 
-
 			<Row className="row--spacing">
 				<Col sm={12} md={6} mdOffset={3}>
 					<h1 className="about__title">New Pathway Education</h1>
 					<p className="about__paragraph">
-						New Pathway Education & Technology Group was founded in 2009. 
 						We provide the most innovative and inspirational one-stop platform 
 						in terms of test preparation and college counseling. We facilitate 
 						self-discovery and maximize student potential while ultimately 
@@ -24,42 +22,8 @@ const About = () => {
 					</p>
 				</Col>
 			</Row>
-			<Row>
-				<Col sm={12} md={4}>
-					<div className="value philosophy">
-						<h1 className="about__title">Philosophy</h1>
-						<p className="about__paragraph">
-							Unlimited potential lies within every student, and the future of our world 
-							will be molded by today's youth. As educators and innovators, it is our duty 
-							to help shape a new pathway for our students' futures.
-						</p>
-					</div>
-				</Col>
-			</Row>
-			<Row>
-				<Col sm={12} md={4} mdOffset={4}>
-					<div className="value mission">
-						<h1 className="about__title">Mission</h1>
-						<p className="about__paragraph">
-							The mission of New Pathway Education & Technology Group is to transform 
-							students lives through education and mentorship.
-						</p>
-					</div>
-				</Col>
-			</Row>
-			<Row>
-				<Col sm={12} md={4} mdOffset={8}>
-					<div className="value vision">
-						<h1 className="about__title">Vision</h1>
-						<p className="about__paragraph">
-							To be the #1 test prep company focused on maximizing every student's potential.
-						</p>
-					</div>
-				</Col>
-			</Row>
-
 			<Row className="row--spacing">
-				<Col md={8} mdOffset={2}>			
+				<Col sm={12} md={7}>			
 					<h1 className="about__title">History</h1>
 					<p className="about__paragraph">
 						New Pathway Education & Technology Group was founded in 2009. Our teachers 
@@ -88,6 +52,53 @@ const About = () => {
 						our students gain personal confidence and ability that transfers to their academic 
 						lives and into their future as they aspire to higher education in the world’s best universities.
 					</p>
+				</Col>
+
+				<Col sm={12} md={4} mdOffset={1}>
+					<h1 className="about__title">Core Values</h1>
+					<ul className="about__paragraph about__list">
+						<li> <b>Passion & Initiative:</b><br/> We have purpose, we are inspired by our mission and thus we are determined </li>
+						<li> <b>Teamwork:</b><br/> We are collaborative, we see our multi-cultural team as an asset and not an obstacle </li>
+						<li> <b>Professionalism:</b><br/> We exercise high levels of professionalism and relay to our clients in this manner so we can produce the best work possible </li>
+						<li> <b>Support & Care:</b><br/> We are for each other, our clients, and our students on a personal level and demonstrate respect for the individual. We are committed to the idea of maximizing each student’s potential </li>
+						<li> <b>Quality & Results:</b><br/> We want to be the best and deliver only quality products and achieve real results. Whatever it takes (WIT) </li>
+					</ul>
+				
+				</Col>
+			</Row>
+
+
+			<Row className="row--spacing">
+				<Col sm={12} md={4}>
+					<div className="value philosophy">
+						<h1 className="about__title">Philosophy</h1>
+						<p className="about__paragraph">
+							Unlimited potential lies within every student, and the future of our world 
+							will be molded by today's youth. As educators and innovators, it is our duty 
+							to help shape a new pathway for our students' futures.
+						</p>
+					</div>
+				</Col>
+			</Row>
+			<Row>
+				<Col sm={12} md={4} mdOffset={4}>
+					<div className="value mission">
+						<h1 className="about__title">Mission</h1>
+						<p className="about__paragraph">
+							The mission of New Pathway Education & Technology Group is to transform 
+							students lives through education and mentorship.
+						</p>
+					</div>
+				</Col>
+			</Row>
+			<Row className="row--spacing">
+				<Col sm={12} md={4} mdOffset={8}>
+					<div className="value vision">
+						<h1 className="about__title">Vision</h1>
+						<p className="about__paragraph">
+							To be the #1 test prep company focused on maximizing every student's potential.
+						</p>
+					</div>
 				</Col>
 			</Row>
 		</div>
