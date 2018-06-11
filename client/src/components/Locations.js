127265c7559976f138f015dceddc40ae67e156d9
Jeremy Han <jeremyhan111@gmail.com>
2018-06-12 00:42:55 +0800

    added location svgs

--- client/src/components/Locations.js
+++ client/src/components/Locations.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { BaiduMap, Marker, asyncWrapper } from 'react-baidu-maps';
 import key from './../config/keys';
-
+import shbuilding1 from './../images/shbuilding1.svg';
+import shbuilding2 from './../images/shbuilding2.svg';
+import shbuilding3 from './../images/shbuilding3.svg';
+import bjbuilding1 from './../images/bjbuilding1.svg';
 
 
 const url = `https://api.map.baidu.com/api?v=2.0&ak=EgeDVs86fhrPgkTNnyzdU3tyCnCthk9z`;
@@ -15,6 +18,7 @@ const Locations = () => {
 			<Row>
 				<Col xs={12} md={2} mdOffset={2}>
 					<div className="campus">
+						<img src={shbuilding1}/>
 						<h1>Xuhui (HQ)</h1>
 						<p className="campus__address">25th Floor, No. 899, Lingling Road, Xuhui District, Shanghai</p>
 						<p className="campus__number">86-21-5432-5782</p>
@@ -22,6 +26,7 @@ const Locations = () => {
 				</Col> 
 				<Col xs={12} md={2}>
 					<div className="campus">
+						<img src={shbuilding2}/>
 						<h1>Gubei</h1>
 						<p className="campus__address">9th Floor, No. 37, South Shuicheng Road, North Wanke Building, Changning District</p>
 						<p className="campus__number">86-21-6275-4553</p>
@@ -29,6 +34,7 @@ const Locations = () => {
 				</Col> 
 				<Col xs={12} md={2}>
 					<div className="campus">
+						<img src={shbuilding3}/>
 						<h1>Pudong</h1>
 						<p className="campus__address">1705, No. 58 Changliu Road, Pudong New District</p>
 						<p className="campus__number">86-21-3382-0351</p>
@@ -36,6 +42,7 @@ const Locations = () => {
 				</Col> 
 				<Col xs={12} md={2}>
 					<div className="campus">
+						<img src={bjbuilding1}/>
 						<h1>Beijing</h1>
 						<p className="campus__address">Lemon Lake Clubhouse, Tianbei Rd, Houshayu Town, Shunyi District</p>
 						<p className="campus__number">86-10-6475-6678</p>
