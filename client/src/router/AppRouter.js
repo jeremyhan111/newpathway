7712dcb8a5c64c0bad1181990561a5d306887228
Jeremy Han <jeremyhan111@gmail.com>
2018-06-19 20:08:34 +0800

    starting to implement lazy loading

--- client/src/router/AppRouter.js
+++ client/src/router/AppRouter.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import asyncComponent from './../components/AsyncComponent';
+
 
 import Header from './../components/Header';
-import Landing from './../components/Landing';
 import Footer from './../components/Footer';
-import Services from './../components/Services';
-import Team from './../components/Team';
-import Contact from './../components/Contact';
-import Elementary from './../components/Elementary';
-import Middle from './../components/Middle';
-import High from './../components/High';
-import Consulting from './../components/Consulting';
 import Class from './../components/Class';
-import About from './../components/About';
-import Jobs from './../components/Jobs';
-import Locations from './../components/Locations';
-import Four from './../components/404';
 
 import sat from './../images/sat.png';
 import act from './../images/act.png';
@@ -23,7 +13,6 @@ import ssat_isee from './../images/ssat_isee.png';
 import ap from './../images/ap.png';
 import toefl from './../images/toefl.png';
 import ela from './../images/ela.jpg';
-
 import sat_course from './../images/sat_course.jpg';
 import act_course from './../images/act_course.jpg';
 import ssat_isee_course from './../images/ssat_isee_course.jpg';
@@ -31,6 +20,50 @@ import ap_course from './../images/ap_course.jpg';
 import toefl_course from './../images/toefl_course.jpg';
 import ela_course from './../images/ela_course.jpg';
 import sat2_course from './../images/sat2_course.jpg';
+
+const Landing = asyncComponent(() =>
+    import('./../components/Landing').then(module => module.default)
+)
+
+const Services = asyncComponent(() =>
+    import('./../components/Services').then(module => module.default)
+)
+
+const Team = asyncComponent(() =>
+    import('./../components/Team').then(module => module.default)
+)
+
+const Contact = asyncComponent(() =>
+    import('./../components/Contact').then(module => module.default)
+)
+
+const Elementary = asyncComponent(() =>
+    import('./../components/Elementary').then(module => module.default)
+)
+
+const Middle = asyncComponent(() =>
+    import('./../components/Middle').then(module => module.default)
+)
+
+const High = asyncComponent(() =>
+    import('./../components/High').then(module => module.default)
+)
+
+const Consulting = asyncComponent(() =>
+    import('./../components/Consulting').then(module => module.default)
+)
+
+const About = asyncComponent(() =>
+    import('./../components/About').then(module => module.default)
+)
+
+const Jobs = asyncComponent(() =>
+    import('./../components/Jobs').then(module => module.default)
+)
+
+const NotFound = asyncComponent(() =>
+    import('./../components/404').then(module => module.default)
+)
 
 const SAT = () => {
 	return (
@@ -57,7 +90,6 @@ const ACT = () => {
 		/>
 	)
 }
-
 
 const SSATISEE = () => {
 	return (
@@ -133,7 +165,6 @@ const AppRouter = () => (
 				<Route path="/contact" component={Contact}/>
 				<Route path="/about" component={About}/> 
 				<Route path="/jobs" component={Jobs}/> 
-				<Route path="/locations" component={Locations}/> 
 
 				<Route path="/ssat-isee" component={SSATISEE}/>
 				<Route path="/ela" component={ELA}/>
@@ -142,7 +173,7 @@ const AppRouter = () => (
 				<Route path="/satii" component={SATII}/>
 				<Route path="/ap" component={AP}/>
 				<Route path="/toefl" component={TOEFL}/>
-				<Route component={Four}/>
+				<Route path="*" component={NotFound}/>
 			</Switch>
 			<Footer/>
 		</div>
